2b6f8838bdfbc8f93fe0e0e70ba6a5559c3bb846
Paul Loyd <pavelko95@gmail.com>
2017-04-24 00:55:07 +0300

    feat(fetcher): check a http status code

--- fetcher/download.rs
+++ fetcher/download.rs
@@ -11,16 +11,21 @@ const USER_AGENT: &str = "Mozilla/5.0 (compatible; chokurei)";
 
 // TODO(loyd): share a session between requests.
 // TODO(loyd): what about redirects?
-// TODO(loyd): check http code of responses.
+// TODO(loyd): a bad http status code isn't IO error.
 
 pub fn channel(handle: &Handle, url: &Url) -> impl Future<Item=Channel, Error=IoError> + 'static {
     get(url.as_ref())
         .header("User-Agent", USER_AGENT)
         .send(handle.clone())
-        .and_then(|res|
-            Channel::read_from(res.body())
+        .and_then(|response| {
+            if !response.is_success() {
+                let cause = format!("Bad status code: {}", response.status_code());
+                return Err(IoError::new(IoErrorKind::Other, cause));
+            }
+
+            Channel::read_from(response.body())
                 .map_err(|cause| IoError::new(IoErrorKind::InvalidData, cause))
-        )
+        })
 }
 
 pub fn document(handle: &Handle, url: &Url) -> impl Future<Item=String, Error=IoError> + 'static {
@@ -28,6 +33,11 @@ pub fn document(handle: &Handle, url: &Url) -> impl Future<Item=String, Error=Io
         .header("User-Agent", USER_AGENT)
         .send(handle.clone())
         .and_then(|response| {
+            if !response.is_success() {
+                let cause = format!("Bad status code: {}", response.status_code());
+                return Err(IoError::new(IoErrorKind::Other, cause));
+            }
+
             let buffer = Vec::from(response);
 
             String::from_utf8(buffer)
