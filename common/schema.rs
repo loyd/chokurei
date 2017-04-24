dcc97e2c051567375bf23027f52e9a60f19e0113
Paul Loyd <pavelko95@gmail.com>
2017-04-24 10:43:44 +0300

    chore(common): replace `infer_schema!` with `table!`

--- common/schema.rs
+++ common/schema.rs
@@ -8,4 +8,31 @@ pub fn establish_connection() -> ConnectionResult<PgConnection> {
     PgConnection::establish(DATABASE_URL)
 }
 
-infer_schema!("postgres://localhost/chokurei");
+table! {
+    feed {
+        id -> Integer,
+        key -> Text,
+        url -> Text,
+        title -> Nullable<Text>,
+        description -> Nullable<Text>,
+        language -> Nullable<Bpchar>,
+        logo -> Nullable<Text>,
+        copyright -> Nullable<Text>,
+        interval -> Nullable<Integer>,
+        augmented -> Nullable<Timestamp>,
+    }
+}
+
+table! {
+    entry {
+        id -> BigInt,
+        key -> Text,
+        feed_id -> Integer,
+        url -> Nullable<Text>,
+        title -> Nullable<Text>,
+        author -> Nullable<Text>,
+        description -> Nullable<Text>,
+        content -> Nullable<Text>,
+        published -> Nullable<Timestamp>,
+    }
+}
