loyd/greeder

feat: migrations and models

common/schema.rs
@@ -1,0 +1,11 @@
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+// TODO(loyd): use UNIX socket in production.
+const DATABASE_URL: &str = "postgres://localhost/greeder";
+
+pub fn establish_connection() -> ConnectionResult<PgConnection> {
+    PgConnection::establish(DATABASE_URL)
+}
+
+infer_schema!("postgres://localhost/greeder");

loyd/chokurei

common/schema.rs
@@ -1,0 +1,11 @@
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+// TODO(loyd): use UNIX socket in production.
+const DATABASE_URL: &str = "postgres://localhost/chokurei";
+
+pub fn establish_connection() -> ConnectionResult<PgConnection> {
+    PgConnection::establish(DATABASE_URL)
+}
+
+infer_schema!("postgres://localhost/chokurei");